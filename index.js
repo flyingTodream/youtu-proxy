4dedcbdc70e2d8a030c4617ad947500231659b28
杨建托 <44236353+flyingTodream@users.noreply.github.com>
2023-01-11 13:07:10 +0800

    fix: 增加脚本2

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const Koa = require('koa');
 const app = new Koa();
+const { getPlayUrl } = require('./getUrl')
 
 app.use(async ctx => {
+    const d = await getPlayUrl()
     ctx.body = 'Hello Vercel';
 });
+
 
 app.listen(3008, () => {
     console.log('3008项目启动')
