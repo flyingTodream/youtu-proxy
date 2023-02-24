1c9232ccae91ea5d30e3979a58a8593a315582f8
杨建托 <44236353+flyingTodream@users.noreply.github.com>
2023-02-24 09:56:59 +0800

    fix: 修改脚本3

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,32 @@
 const router = require('koa-router')()
 const schedule = require('node-schedule');
 
-
 const num = {
-  a: 0
+  'dlc': 1,
+  'cm': 1,
+  'duar': 1
 }
 schedule.scheduleJob('10 * * * * *', () => {
-  num.a = 0
-});
-/* GET home page. */
-router.get('/', async ctx => {
-  num.a = num.a + 1
-  ctx.body = num.a
+  num.dlc = 1;
+  num.cm = 1;
+  num.duar = 1;
 });
 
-router.get('/a', async function (req, res, next) {
-  res.send("hello");
+/* GET home page. */
+router.get('/', async ctx => {
+  ctx.body = 'server is okkkk!'
 });
+
+router.post('/', async function (req, res, next) {
+  const { proName } = req.body;
+  const number = num[proName] || 1;
+  num[proName] = number + 1
+  const d = new Date();
+  res.send(`dist-${proName}-${padNumber(2, Number(d.getMonth() + 1))}${padNumber(2, d.getDate())}-${padNumber(3, number)}`);
+});
+
+function padNumber(num, val) {
+  return val.toString().padStart(num, '0');
+}
 
 module.exports = router;
